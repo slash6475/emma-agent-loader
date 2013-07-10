3de07ff6195b5409f78430b89b20ae2f893580ad
DUHART Clément <duhart@ece.fr>
2013-07-10 11:34:40 +0200

    Fix path problems with empty repository for installation from GIT

--- install.sh
+++ install.sh
@@ -23,12 +23,14 @@ cp -r $THIS/src/emma-agent-loader/* $DEST/
 # Uncompress JSON library
 echo "Copying JSON library ..."
 unzip -q $THIS/dep/json/JSON-java-master.zip -d $DEST/lib/json/
+mkdir $DEST/lib/json/src/
 cp -r $DEST/lib/json/JSON-java-master/* $DEST/lib/json/src/
 rm -r $DEST/lib/json/JSON-java-master
 
 # Uncompress Californium library
 echo "Copying Californium library ..."
 unzip -q $THIS/dep/californium/Californium-coap-13.zip -d $DEST/lib/californium/
+mkdir $DEST/lib/californium/src/
 cp -r $DEST/lib/californium/Californium-coap-13/californium/src/* $DEST/lib/californium/src/
 rm -r $DEST/lib/californium/Californium-coap-13
 
@@ -37,9 +39,11 @@ patch -p5 --directory=$DEST/lib/ --input=$THIS/patch/Californium_CoAP13-to-08_pa
 
 # Copy Californium dependencies
 echo "Copying Californium dependencies ..."
+mkdir $DEST/lib/californium/lib/
 cp -r $THIS/dep/californium/extern/* $DEST/lib/californium/lib/
 
 # Librairies compilation
+mkdir $DEST/lib/dist
 ant jar_libs -f $DEST/build.xml
  
 echo "Done."
